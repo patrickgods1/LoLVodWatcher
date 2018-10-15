42434032793579b5811fb9a880b069c0d8163d9b
patrickgod1 <41496510+patrickgod1@users.noreply.github.com>
2018-10-14 21:20:07 -0700

    Add files via upload

--- LoLVODWatcher.py
+++ LoLVODWatcher.py
@@ -33,28 +33,35 @@ def watchVOD(t=600, multi=1):
 
 	hrefs = [elm.get_attribute('href') for elm in browser.find_elements_by_css_selector("div.VodsList > a")]
 	#print(len(hrefs))
-
-	t = randint(t*60, t*60+99)
+	tLower = t*60 + 5
+	tUpper = t*60 + 99
 	m = 0
-	for i in range(0,len(hrefs)):
-		while i < len(hrefs) and m < multi:
+	i = 0
+	#for i in range(0,len(hrefs)):
+	#while i < len(hrefs) and m < multi:
+	while i < len(hrefs):
+		while m < multi and i < len(hrefs):
 			browser.execute_script(f"window.open('{hrefs[i]}');")
-			#element = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.ID, "video-player-placeholder")))
+			#time.sleep(5)
+			text = 'WATCH REWARDS ARE CORRECTLY SET UP FOR YOUR ACCOUNT'
+			# WATCH REWARDS ARE CORRECTLY SET UP FOR YOUR ACCOUNT
+			#element = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH, '//*[contains(@class, content")]')))
+			#elem = browser.find_element_by_xpath('//*[contains(@class, "content")]')
 			if i == 0:
 				print("Mute and change resolution to 144p")
-				time.sleep(10)
+				time.sleep(5)
 			browser.switch_to_window(browser.window_handles[-1])
+			element = WebDriverWait(browser, 100).until(EC.visibility_of_element_located((By.XPATH, '//*[contains(@class, "content")]')))
 			#browser.get(hrefs[i])
 			print(f"You're watching VOD number: {i+1}")
 			print(f"Switching in {t} seconds")
 			i += 1
 			m += 1
-		time.sleep(t)
-		for x in range(multi):
+		time.sleep(randint(tLower, tUpper))
+		for num in range(multi):
 			browser.close()
 			browser.switch_to.window(browser.window_handles[-1])
-			
-		browser.switch_to.window(browser.window_handles[0])
+		#browser.switch_to.window(browser.window_handles[0])
 		m = 0
 
 	browser.quit()
@@ -66,9 +73,9 @@ def main():
 		if not t.isdigit():
 			print("Sorry, your response must be a number.")
 			continue
-		elif int(t) < 10:
-			print("Sorry, your response must be greater than 10 minutes.")
-			continue
+		# elif int(t) < 10:
+		# 	print("Sorry, your response must be greater than 10 minutes.")
+		# 	continue
 		else:
 			break
 
