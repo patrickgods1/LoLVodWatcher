eef3963e628962ef8cf27fe6aa023aa980bdaeec
Patrick Yu <patrickgod1@aol.com>
2020-06-10 20:00:32 -0700

    Added user configurable settings via config.ini file.

--- LoLVODWatcher.py
+++ LoLVODWatcher.py
@@ -1,22 +1,22 @@
+import configparser
+import os
+from random import randint
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
-import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
-from random import randint
 
 
-def watchVOD(url, t=600, multi=1):
+def watchVOD(userConfig):
     options = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images": 2}
     options.add_experimental_option("prefs", prefs)
     browser = webdriver.Chrome(chrome_options=options)
-    
     browser.get('https://watch.lolesports.com/vods/')
 
     try:
@@ -34,52 +34,53 @@ def watchVOD(url, t=600, multi=1):
     except TimeoutException:
         print('[ERROR] VOD page did not load. Exiting.')
         return 0
-    if not url:
+
+    if not userConfig['Start URL']:
         hrefs = []
-        skip = ['https://watch.lolesports.com/vods/lcs-academy/na_academy_2020_split1',
-                'https://watch.lolesports.com/vods/lck/lck_2020_split1',
-                'https://watch.lolesports.com/vods/lpl/lpl_2020_split1',
-                'https://watch.lolesports.com/vods/worlds/world_championship_2019',
-                'https://watch.lolesports.com/vods/all-star/all_star_2019',
-                'https://watch.lolesports.com/vods/midseason-cup/midseason_cup_2020',
-                'https://watch.lolesports.com/vods/midseason-streamathon/midseason_streamathon_2020']
+        skipRegionsList = [link.strip() for link in userConfig.get('Skip Regions List').split(',') if link]
+
         for league in browser.find_elements_by_css_selector('ul.leagues > li.league > div.info'):
             try:
                 browser.execute_script("arguments[0].click();", league)
                 WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.games > a.game')))
-                if browser.current_url in skip:
-                    print(f'[Skipped] {browser.current_url} is ineligible for watch rewards.')
+                if browser.current_url in skipRegionsList:
+                    print(f"[Skipped] {browser.current_url} is in Skip Regions List")
                     continue
                 # hrefs = hrefs + [elm.get_attribute('href') for elm in browser.find_elements_by_css_selector("a.game.watch-annotated:not(.watched)")]
-                hrefs = hrefs + [elm.get_attribute('href') for elm in browser.find_elements_by_xpath("//a[contains(@class, 'game') and not(contains(@class, 'watched'))]")]
+                hrefs = hrefs + [elm.get_attribute('href') for elm in browser.find_elements_by_xpath("//a[contains(@class, 'game') and not(contains(@class, 'watched'))]") if elm.get_attribute('href') not in userConfig['Skip Vods List']]
             except TimeoutException:
                 print(f'[ERROR] No links found on {browser.current_url}')
                 continue
     else:
         try:
-            browser.get(url)
-            WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.games > a.game')))
+            browser.get(userConfig['Start URL'])
+            # WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.games > a.game')))
+            WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.VodsList')))
         except TimeoutException:
             print('[ERROR] VOD page did not load. Exiting.')
-        hrefs = [elm.get_attribute('href') for elm in browser.find_elements_by_css_selector("div.games > a.game:not(.watched)")]
-        # hrefs = [elm.get_attribute('href') for elm in browser.find_elements_by_xpath("//a[contains(@class, 'game') and not(contains(@class, 'watched'))]")]
+        # hrefs = [elm.get_attribute('href') for elm in browser.find_elements_by_css_selector("div.games > a.game:not(.watched)")]
+        hrefs = [elm.get_attribute('href') for elm in browser.find_elements_by_xpath("//a[contains(@class, 'game') and not(contains(@class, 'watched'))]") if elm.get_attribute('href') not in userConfig['Skip Vods List']]
     print(f'[INFO] Found {len(hrefs)} links to watch.')
     
-    tLower = t * 60 + 5
-    tUpper = t * 60 + 88
+    tLower = userConfig.getint('Time') * 60 + 5
+    tUpper = userConfig.getint('Time') * 60 + 88
     m = 0
     i = 0
     firstVid = True
     watchCount = 0
+    invalidList = []
+    ineligibleList = []
+
     while i < len(hrefs):
         t = randint(tLower, tUpper)
-        while m < multi and i < len(hrefs):
+        while m < userConfig.getint('Batch Size') and i < len(hrefs):
             browser.execute_script(f"window.open('{hrefs[i]}');")
             browser.switch_to_window(browser.window_handles[-1])
             try:
                 WebDriverWait(browser, 1).until(EC.visibility_of_element_located((By.ID, 'video-player')))
             except TimeoutException:
                 print(f'[ERROR] Timed out waiting for video player to load: {hrefs[i]}')
+                invalidList.append(hrefs[i])
                 browser.close()
                 browser.switch_to.window(browser.window_handles[-1])
                 i += 1
@@ -110,12 +111,13 @@ def watchVOD(url, t=600, multi=1):
                 WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, 'content')))
             except TimeoutException:
                 print(f'[Skipped] {hrefs[i]} is ineligible for watch rewards.')
+                ineligibleList.append(hrefs[i])
                 browser.close()
                 browser.switch_to.window(browser.window_handles[-1])
                 i += 1
                 continue
-            print(f'[INFO] You are watching VOD number {i+1}: "{hrefs[i]}"')
-            print(f'[INFO] Switching in {t} seconds')
+            print(f'[INFO] You are watching VOD number {watchCount+1}: "{hrefs[i]}"')
+            print(f'[INFO] Watching for {t} seconds')
             i += 1
             m += 1
             watchCount += 1
@@ -131,37 +133,86 @@ def watchVOD(url, t=600, multi=1):
 
     browser.quit()
     print(f'[INFO] You have watched {watchCount} VODs')
+    if userConfig.getboolean('Output Ineligible Links'):
+        if ineligibleList:
+            print(f'[INFO] Outputting list of videos inelgible for rewards:')
+            for l in ineligibleList:
+                print(l)
+        else:
+            print(f'[INFO] Outputting list of videos inelgible for rewards: None found')
+
+    if userConfig.getboolean('Output Invalid Links'):
+        if invalidList:
+            print(f'[INFO] Outputting list of links with no VODs found:')
+            for l in invalidList:
+                print(l)
+        else:
+            print(f'[INFO] Outputting list of links with no VODs found: None found')
 
 
 def main():
-    while True:
-        t = input(f'How long do you want to watch each VOD: (10 minutes minimum) ')
-        if not t.isdigit():
-            print(f'[ERROR] Response must be a number.')
-            continue
-        elif int(t) < 10:
-            print(f'[ERROR] Response must be greater than 5 minutes.')
-            continue
-        else:
-            break
+    config = configparser.ConfigParser()
+    try:
+        with open('config.ini') as f:
+            config.read(f)
+    except IOError:
+        print('[Info] Cannot find existing configuration file. Creating a new config.ini file.')
+        config['DEFAULT'] = {'User Input Mode': 'true',
+                                'Start URL': '',
+                                'Batch Size': 2,
+                                'Time': 15,
+                                'Output Ineligible Links': 'false',
+                                'Output Invalid Links': 'false',
+                                'Skip Regions List': '',
+                                'Skip VODs List': ''}
+        config['User Configuration'] = {'User Input Mode': 'true',
+                                'Start URL': '',
+                                'Batch Size': 5,
+                                'Time': 15,
+                                'Output Ineligible Links': 'false',
+                                'Output Invalid Links': 'false',
+                                'Skip Regions List': '',
+                                'Skip VODs List': ''}
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
 
-    while True:
-        multi = input(f'How many VODs do you want to watch simultaneously? ')
-        if not multi.isdigit():
-            print(f'[ERROR] Response must be a number.')
-            continue
-        elif int(multi) < 1:
-            print(f'[ERROR] Response must greater than zero.')
-            continue
-        else:
-            break
+    config.read('config.ini')
+    userConfig = config['User Configuration']
+    if userConfig.getboolean('User Input Mode'): # User input mode is on.
+        while True:
+            t = input(f'How long do you want to watch each VOD: (10 minutes minimum) ')
+            if not t.isdigit():
+                print(f'[ERROR] Response must be a number.')
+                continue
+            elif int(t) < 10:
+                print(f'[ERROR] Response must be greater than 5 minutes.')
+                continue
+            else:
+                break
+        userConfig['Time'] = t
 
-    url = input(f'URL to VODs page (Press ENTER to search through all links): ')
-    if 'https://watch.lolesports.com/vods/' in url:
-        watchVOD(url, int(t), int(multi))
-    else:
-        print(f"[INFO] No URL entered. Searching all leagues for links.")
-        watchVOD(None, int(t), int(multi))
+        while True:
+            batchSize = input(f'How many VODs do you want to watch simultaneously? ')
+            if not batchSize.isdigit():
+                print(f'[ERROR] Response must be a number.')
+                continue
+            elif int(batchSize) < 1:
+                print(f'[ERROR] Response must greater than zero.')
+                continue
+            else:
+                break
+        userConfig['Batch Size'] = batchSize
+
+        url = input(f'URL to VODs page (Press ENTER to search through all links): ')
+        if 'https://watch.lolesports.com/vods/' in url:
+            userConfig['Start URL'] = url
+        else:
+            print(f"[INFO] No URL entered. Searching all leagues for links.")
+        watchVOD(userConfig)
+
+    else:   # User input is off. Use config.ini settings.
+        print('[INFO] Using config.ini file settings.')
+        watchVOD(userConfig)
 
 
 if __name__ == "__main__":
